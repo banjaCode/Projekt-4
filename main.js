85212c7ea4c350714f7a0c7d82178b57607e7fd6
benjamin eze <benjamin10eze@gmail.com>
2021-05-10 13:36:48 +0200

    jag har jobbat med javascriptet. Gjort det möjligt att byta lista

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-"use strict"; 
+"use strict";
 
-let todos = ["Köp kattmat", "Töm kattlådan", "Klappa katten"];
-let listRoot = document.querySelector("#list-root");
+// todolist
+let todosMåndag = [""];
+let todosTisdag = [""];
+let todosOnsdag = [""];
+let todosTorsdag = [""];
+let todosFredag = [""];
+let todosLördag = [""];
+let todosSöndag = [""];
+let todosDagar = [
+  todosMåndag,
+  todosTisdag,
+  todosOnsdag,
+  todosTorsdag,
+  todosFredag,
+  todosLördag,
+  todosSöndag,
+];
+let lista = 0;
+let listRoot = document.querySelectorAll(".list-root");
 let listForm = document.querySelector("[data-list-form]");
 let listInput = document.querySelector("[data-list-input]");
 
@@ -10,18 +27,16 @@ listForm.addEventListener("submit", (e) => {
   if (listInput.value.trim() === "") {
     return;
   }
-  todos.push(listInput.value.trim());
+  todosDagar[lista].push(listInput.value.trim());
   updateList();
   listInput.value = "";
 });
-
 
 function todoList(items) {
   let list = document.createElement("ul");
   items.forEach((item) => {
     let todoListItem = document.createElement("li");
     todoListItem.innerText = item;
-    todoListItem.classList.add("todo-list-item");
     todoListItem.addEventListener("click", removeItem);
     list.append(todoListItem);
   });
@@ -30,13 +45,17 @@ function todoList(items) {
 
 function removeItem(event) {
   let itemToRemove = event.target.innerText;
-  todos = todos.filter((item) => item !== itemToRemove);
+  todosDagar[lista] = todosDagar[lista].filter((item) => item !== itemToRemove);
   updateList();
 }
 
 function updateList() {
-  listRoot.innerHTML = "";
-  listRoot.append(todoList(todos));
+  listRoot[lista].innerHTML = "";
+  listRoot[lista].append(todoList(todosDagar[lista]));
 }
 
 updateList();
+
+function setDay(day){
+lista = day
+}
