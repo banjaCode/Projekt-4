5fa0d4800af8ac7a2b686307dabbe8cdcc9479b8
benjamin eze <benjamin10eze@gmail.com>
2021-05-12 21:45:08 +0200

    har lagt till styling till listan.

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 "use strict";
 
 // todolist
-let todosMåndag = [""];
-let todosTisdag = [""];
-let todosOnsdag = [""];
-let todosTorsdag = [""];
-let todosFredag = [""];
-let todosLördag = [""];
-let todosSöndag = [""];
+let todosMåndag = [];
+let todosTisdag = [];
+let todosOnsdag = [];
+let todosTorsdag = [];
+let todosFredag = [];
+let todosLördag = [];
+let todosSöndag = [];
 let todosDagar = [
   todosMåndag,
   todosTisdag,
@@ -21,6 +21,15 @@ let lista = 0;
 let listRoot = document.querySelectorAll(".list-root");
 let listForm = document.querySelector("[data-list-form]");
 let listInput = document.querySelector("[data-list-input]");
+let ButtonDag = document.querySelectorAll(".buttonDag");
+let förraLista;
+
+for(let i = 0; i < 7; i++){
+  ButtonDag[i].addEventListener("click", (e) =>{
+    setDay(i);
+    changeColor(förraLista);
+  } );
+  }
 
 listForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -57,5 +66,14 @@ function updateList() {
 updateList();
 
 function setDay(day){
+förraLista = lista
 lista = day
 }
+
+function changeColor(förraLista) {
+  listRoot[förraLista].style.backgroundColor = "#e5e5e5";
+  listRoot[lista].style.backgroundColor = "red";
+}
+
+changeColor(0);
+
